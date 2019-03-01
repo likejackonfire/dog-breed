422eccd6412ffa9d746cb3dbea277a6dbecb7eac
Enrique Montemayor <eemontemayor@live.com>
2019-03-01 12:20:06 -0600

    tweadked

--- script.js
+++ script.js
@@ -1,8 +1,18 @@
-// // Create an app that lets users choose to display between 1 and 50 random dog images, 
-// then prints the results to the console. 
-// The app should feature a form with a required input where users indicate the number of images to retrieve,
-// the input should default to 3. Use the endpoint described in the "DISPLAY MULTIPLE RANDOM IMAGES FROM ALL DOGS COLLECTION" 
-// section of this page of the DogAPI docs.
+'use strict';
+
+
+
+function watchForm(){
+    // // this function listens to the input element in the html. 
+    // default is set to three, user can choose between 1 and 50  
+    $('form').submit(function(event){
+        event.preventDefault();
+        const userNumber= $('.number-value').val();
+        console.log(number);
+        getDogImages(userNumber);
+    })
+
+}
 
 function getDogImages(userNumber){
     fetch(`https://dog.ceo/api/breeds/image/random/${userNumber}`)
@@ -23,22 +33,7 @@ function validateNumber(num){
 }
 
 
-function watchForm(){
-    // // this function listens to the input element in the html. 
-    // default is set to three, user can choose between 1 and 50  
-    $('form').on('submit', function(event){
-        event.preventDefault();
-        const userNumber= $('.number-value').val();
-        console.log(number);
-        validateNumber(userNumber);
-        getDogImages(userNumber);
-    })
 
-}
-
-function renderHtml(){
-
-}
 
 $(function() {
     console.log('App loaded! good job!');
