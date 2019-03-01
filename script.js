2a92c89afc9b39656b6b412672fa679e21303f2a
Enrique Montemayor <eemontemayor@live.com>
2019-03-01 15:15:15 -0600

    debugging js file

--- script.js
+++ script.js
@@ -2,12 +2,13 @@
 
 const store = {
     number: 0,
+    dogArray:[],
 }
 
 function watchForm(){
     // // this function listens to the input element in the html. 
     // default is set to three, user can choose between 1 and 50  
-    const userNumber = 3;
+   
     $('form').on('submit', function(event){
         event.preventDefault();
         store.number= $('.number-value').val();
@@ -19,23 +20,28 @@ function watchForm(){
 function getDogImages(num){
     fetch(`https://dog.ceo/api/breeds/image/random/${num}`)
         .then(response => response.json())
-        .then(responseJson => 
-            displayResults(responseJson))
+        .then(responseJson => displayResults(responseJson))
             .catch(error => alert('Something went wrong. Try again later.'));
 }
 
+function makeDogArray(jsonObj){
+    store.dogArray = jsonObj;
+    console.log('ok');
+}
+
+function makeHtml(){
+    for(let x = 0; x < store.dogArray.length; x++){
+        store.dogArray.push(`<img src="${store.dogArray[x]}" class="results">`)
+}
+    return store.dogArray.join('');
+}    
+
 function displayResults(responseJson){
-    for(let x = 0; x < responseJson.message.length; x++);
-    console.log(responseJson);
     $('.results').replaceWith(
         `<img src="${responseJson.message}" class="results">`
-      )
+    )}
 
-}
 
-function validateNumber(num){
-    // make sure number is between 1 and 50
-}
 
 
 
