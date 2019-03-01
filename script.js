11c6264984cdd728539c68db0783d10a7bde88a7
Enrique Montemayor <eemontemayor@live.com>
2019-03-01 15:29:13 -0600

    finished task 2

--- script.js
+++ script.js
@@ -20,30 +20,26 @@ function watchForm(){
 function getDogImages(num){
     fetch(`https://dog.ceo/api/breeds/image/random/${num}`)
         .then(response => response.json())
-        .then(responseJson => displayResults(responseJson))
+        .then(responseJson => makeDogArray(responseJson)).then(displayResults)
             .catch(error => alert('Something went wrong. Try again later.'));
 }
 
-function makeDogArray(jsonObj){
-    store.dogArray = jsonObj;
-    console.log('ok');
+function makeDogArray(responseJson){
+    store.dogArray = responseJson.message;
+    console.log(store.dogArray);
+   
 }
 
 function makeHtml(){
+    let newArr=[];
     for(let x = 0; x < store.dogArray.length; x++){
-        store.dogArray.push(`<img src="${store.dogArray[x]}" class="results">`)
+        newArr.push(`<img src="${store.dogArray[x]}" class="results">`)
 }
-    return store.dogArray.join('');
+   return newArr;
 }    
 
 function displayResults(responseJson){
-    $('.results').replaceWith(
-        `<img src="${responseJson.message}" class="results">`
-    )}
-
-
-
-
+    $('.results').replaceWith(makeHtml())}
 
 
 $(function() {
